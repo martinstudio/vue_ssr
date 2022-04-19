94df590aa9482c32dfda156fbb6a051c2dd750e8
马丁 <php_martin@163.com>
2022-04-19 14:39:42 +0800

    客户端打包

--- README.md
+++ README.md
@@ -5,3 +5,13 @@
 ## 1.2 koa@2 和 @koa/router@8
 
 ## 1.3 nodemon@2
+
+## 1.4 webpack@4(打包) webpack-cli@3(解析参数) webpack-dev-server@3(webpack开发环境)
+
+## 1.5 vue-loader@15(解析.vue文件) vue-style-loader@4(支持服务端渲染) css-loader@3(处理css) vue-template-compiler(解析template标签)
+
+## 1.6 @babel/core@7(babel核心模块) @babel/preset-env@7(高级语法转成低级语法)
+
+## 1.7 babel-loader@8(解析js文件)
+
+## 1.8 webpack-merge@4(webpack合并文件)
---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<!--  -->
+<template>
+  <div id="App">
+    <Bar />
+    <Foo />
+  </div>
+</template>
+
+<script>
+import Bar from "./components/Bar.vue";
+import Foo from "./components/Foo.vue";
+export default {
+  components: {
+    Bar,
+    Foo,
+  },
+  data() {
+    return {};
+  },
+  //生命周期 - 创建完成（访问当前this实例）
+  created() {},
+  //生命周期 - 挂载完成（访问DOM元素）
+  mounted() {},
+};
+</script>
+<style scoped>
+/* @import url(); 引入css类 */
+</style>
